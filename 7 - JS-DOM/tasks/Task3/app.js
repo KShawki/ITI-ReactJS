e138aa94a7724791f97fbe78a9952d52b5c37bb7
Khaled Shawki <khalid.i.shawki@gmail.com>
2023-08-22 10:18:40 +0300

    Add Task 3

---
+++ 7 - JS-DOM/tasks/Task3/app.js
@@ -0,0 +1,37 @@
+var element = document.getElementById("PAR"); 
+
+function ChangeFont(fontFamily) {
+  element.style.fontFamily = fontFamily; 
+}; 
+
+function ChangeAlign(textAlign) {
+  element.style.textAlign = textAlign; 
+}
+
+function ChangeHeight(lineHeight) {
+  element.style.lineHeight = lineHeight
+}
+
+function ChangeLSpace(letterSpacing) {
+  element.style.letterSpacing = letterSpacing; 
+}
+
+function ChangeIndent(textIndent) {
+  element.style.textIndent = textIndent;
+}
+
+function ChangeTransform(textTransform) {
+  element.style.textTransform = textTransform
+}
+
+function ChangeDecorate(textDecoration) {
+  element.style.textDecoration = textDecoration; 
+}
+
+function ChangeBorder(textBorder) {
+  element.style.border = textBorder 
+}
+
+function ChangeBorderColor(Bordercolor) {
+  element.style.borderColor = Bordercolor; 
+}
